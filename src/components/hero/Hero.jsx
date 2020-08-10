7db6316e91f16ff4ba8fbc5ddf0d79106b0e823a
abhijeet gupta <abhijeetg40@gmail.com>
2020-08-10 21:59:28 +0530

    ui

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -6,11 +6,11 @@ import './styles.css'
 function hero(props) {
     return (
         <div className="hero">
-           <div className="container d-flex">
-           <img src={hero_image} alt="hero"/>
-           <h2 className="mt-5 pt-5 hero__text">Read news by giving command</h2>
+           <div className="container ">
+           <img src={hero_image} alt="hero" className="img-fluid img-responsivegi"/>
+           {/* <h2 className="mt-5 pt-5 hero__text">Read news by giving command</h2>
 
-        
+         */}
 
        
             
